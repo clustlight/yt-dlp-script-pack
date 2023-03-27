e911e72eb6096f5dc2e4f71ab446d05bfe6b2840
quadseed <contact@quadseed.net>
2023-03-28 05:24:02 +0900

    add priority option

--- !!!-setup.bat
+++ !!!-setup.bat
@@ -3,7 +3,7 @@ chcp 65001 > nul
 
 echo [Info] (1/3) ffmpegをダウンロードします
 timeout 3
-bitsadmin /transfer "ffmpeg" https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip %~dp0/ffmpeg-build.zip
+bitsadmin /transfer "ffmpeg" /priority FOREGROUND https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip %~dp0/ffmpeg-build.zip
 timeout 1
 echo [Info] (2/3) ffmpegを解凍します
 call powershell -command "Expand-Archive ffmpeg-build.zip"
@@ -14,7 +14,7 @@ rmdir /Q /S ffmpeg-build
 
 echo [Info] (3/3) yt-dlp.exeをダウンロードします
 timeout 3
-bitsadmin /transfer "yt-dlp" https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe %~dp0/yt-dlp.exe
+bitsadmin /transfer "yt-dlp" /priority FOREGROUND https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe %~dp0/yt-dlp.exe
 
 
 echo [Info] セットアップ完了
