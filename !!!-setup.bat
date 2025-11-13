b0fd2a904457843fcfdcc5526e163a3f23468047
quadseed <contact@quadseed.net>
2025-11-13 19:15:04 +0900

    Refactor setup script to include deno download and update progress messages

--- !!!-setup.bat
+++ !!!-setup.bat
@@ -19,8 +19,10 @@ IF exist .\yt-dlp.exe (
     )
 )
 
-echo [Info] (1/3) ffmpegをダウンロードします
+echo [Info] yt-dlpのセットアップを開始します
 timeout 3
+
+echo [Info] (1/5) ffmpegをダウンロードします
 
 if %curlExists% equ 0 (
     curl -L -o %~dp0/bin/ffmpeg-build.zip https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip
@@ -28,16 +30,30 @@ if %curlExists% equ 0 (
     bitsadmin /transfer "ffmpeg" /priority FOREGROUND https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip %~dp0/bin/ffmpeg-build.zip
 )
 
-timeout 1
-echo [Info] (2/3) ffmpegを解凍します
+echo [Info] (2/5) ffmpegを解凍します
 call powershell -command "Expand-Archive ffmpeg-build.zip"
 del /Q ffmpeg-build.zip
 
 move .\ffmpeg-build\ffmpeg-master-latest-win64-gpl-shared\bin\* . > nul
 rmdir /Q /S ffmpeg-build
 
-echo [Info] (3/3) yt-dlp.exeをダウンロードします
-timeout 3
+echo [Info] (3/5) denoをダウンロードします
+
+if %curlExists% equ 0 (
+    curl -L -o %~dp0/bin/deno.zip https://github.com/denoland/deno/releases/latest/download/deno-x86_64-pc-windows-msvc.zip
+) else (
+    bitsadmin /transfer "deno" /priority FOREGROUND https://github.com/denoland/deno/releases/latest/download/deno-x86_64-pc-windows-msvc.zip %~dp0/bin/deno.zip
+)
+
+
+echo [Info] (4/5) denoを解凍します
+call powershell -command "Expand-Archive deno.zip"
+del /Q deno.zip
+
+move .\deno\* . > nul
+rmdir /Q /S deno
+
+echo [Info] (5/5) yt-dlp.exeをダウンロードします
 
 if %curlExists% equ 0 (
     curl -L -o %~dp0/bin/yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
